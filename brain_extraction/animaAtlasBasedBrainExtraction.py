cfd69b21983799ec3ad2cb46b7cd88c0ab1cce58
Olivier Commowick <olivier.commowick@inria.fr>
2018-07-23 15:03:08 +0200

    Better masking process in two steps, rough first then fine

--- brain_extraction/animaAtlasBasedBrainExtraction.py
+++ brain_extraction/animaAtlasBasedBrainExtraction.py
@@ -11,7 +11,7 @@ else :
 import glob
 import os
 import shutil
-from subprocess import call
+from subprocess import call, check_output
 
 configFilePath = os.path.expanduser("~") + "/.anima/config.txt"
 if not os.path.exists(configFilePath) :
@@ -23,6 +23,12 @@ configParser.read(configFilePath)
 
 animaDir = configParser.get("anima-scripts",'anima')
 animaExtraDataDir = configParser.get("anima-scripts",'extra-data-root')
+animaPyramidalBMRegistration = os.path.join(animaDir, "animaPyramidalBMRegistration")
+animaDenseSVFBMRegistration = os.path.join(animaDir, "animaDenseSVFBMRegistration")
+animaTransformSerieXmlGenerator = os.path.join(animaDir, "animaTransformSerieXmlGenerator")
+animaApplyTransformSerie = os.path.join(animaDir, "animaApplyTransformSerie")
+animaConvertImage = os.path.join(animaDir, "animaConvertImage")
+animaMaskImage = os.path.join(animaDir, "animaMaskImage")
 
 if len(sys.argv) < 2:
 	print('Computes the brain mask of images given in input by registering a known atlas on it. Their output is prefix_brainMask.nrrd and prefix_masked.nrrd.')
@@ -30,6 +36,7 @@ if len(sys.argv) < 2:
 
 numImages = len(sys.argv) - 1
 atlasImage = animaExtraDataDir + "icc_atlas/Reference_T1.nrrd"
+atlasImageMasked = animaExtraDataDir + "icc_atlas/Reference_T1_masked.nrrd"
 iccImage = animaExtraDataDir + "icc_atlas/BrainMask.nrrd"
 
 for brainImage in sys.argv[1:] :
@@ -40,24 +47,58 @@ for brainImage in sys.argv[1:] :
 	if os.path.splitext(brainImage)[1] == '.gz' :
 		brainImagePrefix = os.path.splitext(brainImagePrefix)[0]
 
-	command = [animaDir + "animaPyramidalBMRegistration","-m",atlasImage,"-r",brainImage,"-o",brainImagePrefix + "_rig.nrrd","-O",brainImagePrefix + "_rig_tr.txt","-p","4","-l","1","--sp","3"]
+	# Decide on whether to use large image setting or small image setting
+	command = [animaConvertImage, "-i", brainImage, "-I"]
+	convert_output = check_output(command)
+	size_info = convert_output.split('\n')[1].split('[')[1].split(']')[0]
+	large_image = False
+	for i in range(0, 3):
+		size_tmp = int(size_info.split(', ')[i])
+		if size_tmp >= 256:
+			large_image = True
+			break
+
+	pyramidOptions = ["-p","4","-l","1"]
+	if large_image:
+		pyramidOptions = ["-p","5","-l","2"]
+
+	# Rough mask with whole brain
+	command = [animaPyramidalBMRegistration,"-m",atlasImage,"-r",brainImage,"-o",brainImagePrefix + "_rig.nrrd","-O",brainImagePrefix + "_rig_tr.txt","--sp","3"] + pyramidOptions
 	call(command)
 
-	command = [animaDir + "animaPyramidalBMRegistration","-m",atlasImage,"-r",brainImage,"-o",brainImagePrefix + "_aff.nrrd","-O",brainImagePrefix + "_aff_tr.txt","-i",brainImagePrefix + "_rig_tr.txt","-p","4","-l","1","--sp","3","--ot","2"]
+	command = [animaPyramidalBMRegistration,"-m",atlasImage,"-r",brainImage,"-o",brainImagePrefix + "_aff.nrrd","-O",brainImagePrefix + "_aff_tr.txt","-i",brainImagePrefix + "_rig_tr.txt","--sp","3","--ot","2"] + pyramidOptions
 	call(command)
 
-	command = [animaDir + "animaDenseSVFBMRegistration","-r",brainImage,"-m",brainImagePrefix + "_aff.nrrd","-o",brainImagePrefix + "_nl.nrrd","-O",brainImagePrefix + "_nl_tr.nrrd","-p","4","-l","1","--sr","1"]
+	command = [animaDenseSVFBMRegistration,"-r",brainImage,"-m",brainImagePrefix + "_aff.nrrd","-o",brainImagePrefix + "_nl.nrrd","-O",brainImagePrefix + "_nl_tr.nrrd","--sr","1"] + pyramidOptions
 	call(command)
 
-	command = [animaDir + "animaTransformSerieXmlGenerator","-i",brainImagePrefix + "_aff_tr.txt","-i",brainImagePrefix + "_nl_tr.nrrd","-o",brainImagePrefix + "_nl_tr.xml"]
+	command = [animaTransformSerieXmlGenerator,"-i",brainImagePrefix + "_aff_tr.txt","-i",brainImagePrefix + "_nl_tr.nrrd","-o",brainImagePrefix + "_nl_tr.xml"]
 	call(command)
 
-	command = [animaDir + "animaApplyTransformSerie","-i",iccImage,"-t",brainImagePrefix + "_nl_tr.xml","-g",brainImage,"-o",brainImagePrefix + "_brainMask.nrrd","-n","nearest"]
+	command = [animaApplyTransformSerie,"-i",iccImage,"-t",brainImagePrefix + "_nl_tr.xml","-g",brainImage,"-o",brainImagePrefix + "_rough_brainMask.nrrd","-n","nearest"]
 	call(command)
 
-	command = [animaDir + "animaMaskImage","-i",brainImage,"-m",brainImagePrefix + "_brainMask.nrrd","-o",brainImagePrefix + "_masked.nrrd"]
+	command = [animaMaskImage,"-i",brainImage,"-m",brainImagePrefix + "_rough_brainMask.nrrd","-o",brainImagePrefix + "_rough_masked.nrrd"]
+	call(command)
+
+	brainImageRoughMasked = brainImagePrefix + "_rough_masked.nrrd"
+	# Fine mask with masked brain
+	command = [animaPyramidalBMRegistration,"-m",atlasImageMasked,"-r",brainImageRoughMasked,"-o",brainImagePrefix + "_rig.nrrd","-O",brainImagePrefix + "_rig_tr.txt","--sp","3"] + pyramidOptions
+	call(command)
+
+	command = [animaPyramidalBMRegistration,"-m",atlasImageMasked,"-r",brainImageRoughMasked,"-o",brainImagePrefix + "_aff.nrrd","-O",brainImagePrefix + "_aff_tr.txt","-i",brainImagePrefix + "_rig_tr.txt","--sp","3","--ot","2"] + pyramidOptions
+	call(command)
+
+	command = [animaDenseSVFBMRegistration,"-r",brainImageRoughMasked,"-m",brainImagePrefix + "_aff.nrrd","-o",brainImagePrefix + "_nl.nrrd","-O",brainImagePrefix + "_nl_tr.nrrd","--sr","1"] + pyramidOptions
+	call(command)
+
+	command = [animaApplyTransformSerie,"-i",iccImage,"-t",brainImagePrefix + "_nl_tr.xml","-g",brainImage,"-o",brainImagePrefix + "_brainMask.nrrd","-n","nearest"]
+	call(command)
+
+	command = [animaMaskImage,"-i",brainImage,"-m",brainImagePrefix + "_brainMask.nrrd","-o",brainImagePrefix + "_masked.nrrd"]
 	call(command)
 
 map(os.remove, glob.glob("*_rig*"))
 map(os.remove, glob.glob("*_aff*"))
 map(os.remove, glob.glob("*_nl*"))
+map(os.remove, glob.glob("*_rough*"))
