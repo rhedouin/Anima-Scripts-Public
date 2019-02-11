96ab70d067184920a458dd13e1152483a0034f34
Olivier Commowick <olivier.commowick@inria.fr>
2019-02-11 11:34:01 +0100

    Change brain extraction script to have an option for secondary brain extraction (also correct bug for brain size evaluation

--- brain_extraction/animaAtlasBasedBrainExtraction.py
+++ brain_extraction/animaAtlasBasedBrainExtraction.py
@@ -2,6 +2,7 @@
 # Warning: works only on unix-like systems, not windows where "python animaAtlasBasedBrainExtraction.py ..." has to be run
 
 import sys
+import argparse
 
 if sys.version_info[0] > 2:
     import configparser as ConfParser
@@ -10,7 +11,7 @@ else:
 
 import glob
 import os
-import shutil
+from shutil import copyfile
 from subprocess import call, check_output
 
 configFilePath = os.path.expanduser("~") + "/.anima/config.txt"
@@ -30,66 +31,74 @@ animaApplyTransformSerie = os.path.join(animaDir, "animaApplyTransformSerie")
 animaConvertImage = os.path.join(animaDir, "animaConvertImage")
 animaMaskImage = os.path.join(animaDir, "animaMaskImage")
 
-if len(sys.argv) < 2:
-    print(
-        'Computes the brain mask of images given in input by registering a known atlas on it. Their output is prefix_brainMask.nrrd and prefix_masked.nrrd.')
-    quit()
+# Argument parsing
+parser = argparse.ArgumentParser(
+    description="Computes the brain mask of images given in input by registering a known atlas on it. Their output is prefix_brainMask.nrrd and prefix_masked.nrrd")
+
+parser.add_argument('-S', '--second-step', action='store_true',
+                    help="Perform second step of atlas based cropping (might crop part of the external part of the brain)")
+
+parser.add_argument('-i', '--input', type=str, required=True, help='DWI file to process')
+
+args = parser.parse_args()
 
 numImages = len(sys.argv) - 1
 atlasImage = animaExtraDataDir + "icc_atlas/Reference_T1.nrrd"
 atlasImageMasked = animaExtraDataDir + "icc_atlas/Reference_T1_masked.nrrd"
 iccImage = animaExtraDataDir + "icc_atlas/BrainMask.nrrd"
 
-for brainImage in sys.argv[1:]:
-    print("Brain masking image: " + brainImage)
+brainImage = args.input
+print("Brain masking image: " + brainImage)
 
-    # Get floating image prefix
-    brainImagePrefix = os.path.splitext(brainImage)[0]
-    if os.path.splitext(brainImage)[1] == '.gz':
-        brainImagePrefix = os.path.splitext(brainImagePrefix)[0]
+# Get floating image prefix
+brainImagePrefix = os.path.splitext(brainImage)[0]
+if os.path.splitext(brainImage)[1] == '.gz':
+    brainImagePrefix = os.path.splitext(brainImagePrefix)[0]
 
-    # Decide on whether to use large image setting or small image setting
-    command = [animaConvertImage, "-i", brainImage, "-I"]
-    convert_output = check_output(command)
-    size_info = convert_output.split('\n')[1].split('[')[1].split(']')[0]
-    large_image = False
-    for i in range(0, 3):
-        size_tmp = int(size_info.split(', ')[i])
-        if size_tmp >= 256:
-            large_image = True
-            break
+# Decide on whether to use large image setting or small image setting
+command = [animaConvertImage, "-i", brainImage, "-I"]
+convert_output = check_output(command)
+size_info = convert_output.split('\n')[1].split('[')[1].split(']')[0]
+large_image = False
+for i in range(0, 3):
+    size_tmp = int(size_info.split(', ')[i])
+    if size_tmp >= 350:
+        large_image = True
+        break
 
-    pyramidOptions = ["-p", "4", "-l", "1"]
-    if large_image:
-        pyramidOptions = ["-p", "5", "-l", "2"]
+pyramidOptions = ["-p", "4", "-l", "1"]
+if large_image:
+    pyramidOptions = ["-p", "5", "-l", "2"]
 
-    # Rough mask with whole brain
-    command = [animaPyramidalBMRegistration, "-m", atlasImage, "-r", brainImage, "-o", brainImagePrefix + "_rig.nrrd",
-               "-O", brainImagePrefix + "_rig_tr.txt", "--sp", "3"] + pyramidOptions
-    call(command)
+# Rough mask with whole brain
+command = [animaPyramidalBMRegistration, "-m", atlasImage, "-r", brainImage, "-o", brainImagePrefix + "_rig.nrrd",
+           "-O", brainImagePrefix + "_rig_tr.txt", "--sp", "3"] + pyramidOptions
+call(command)
 
-    command = [animaPyramidalBMRegistration, "-m", atlasImage, "-r", brainImage, "-o", brainImagePrefix + "_aff.nrrd",
-               "-O", brainImagePrefix + "_aff_tr.txt", "-i", brainImagePrefix + "_rig_tr.txt", "--sp", "3", "--ot",
-               "2"] + pyramidOptions
-    call(command)
+command = [animaPyramidalBMRegistration, "-m", atlasImage, "-r", brainImage, "-o", brainImagePrefix + "_aff.nrrd",
+           "-O", brainImagePrefix + "_aff_tr.txt", "-i", brainImagePrefix + "_rig_tr.txt", "--sp", "3", "--ot",
+           "2"] + pyramidOptions
+call(command)
 
-    command = [animaDenseSVFBMRegistration, "-r", brainImage, "-m", brainImagePrefix + "_aff.nrrd", "-o",
-               brainImagePrefix + "_nl.nrrd", "-O", brainImagePrefix + "_nl_tr.nrrd", "--sr", "1"] + pyramidOptions
-    call(command)
+command = [animaDenseSVFBMRegistration, "-r", brainImage, "-m", brainImagePrefix + "_aff.nrrd", "-o",
+           brainImagePrefix + "_nl.nrrd", "-O", brainImagePrefix + "_nl_tr.nrrd", "--sr", "1"] + pyramidOptions
+call(command)
 
-    command = [animaTransformSerieXmlGenerator, "-i", brainImagePrefix + "_aff_tr.txt", "-i",
-               brainImagePrefix + "_nl_tr.nrrd", "-o", brainImagePrefix + "_nl_tr.xml"]
-    call(command)
+command = [animaTransformSerieXmlGenerator, "-i", brainImagePrefix + "_aff_tr.txt", "-i",
+           brainImagePrefix + "_nl_tr.nrrd", "-o", brainImagePrefix + "_nl_tr.xml"]
+call(command)
 
-    command = [animaApplyTransformSerie, "-i", iccImage, "-t", brainImagePrefix + "_nl_tr.xml", "-g", brainImage, "-o",
-               brainImagePrefix + "_rough_brainMask.nrrd", "-n", "nearest"]
-    call(command)
+command = [animaApplyTransformSerie, "-i", iccImage, "-t", brainImagePrefix + "_nl_tr.xml", "-g", brainImage, "-o",
+           brainImagePrefix + "_rough_brainMask.nrrd", "-n", "nearest"]
+call(command)
 
-    command = [animaMaskImage, "-i", brainImage, "-m", brainImagePrefix + "_rough_brainMask.nrrd", "-o",
-               brainImagePrefix + "_rough_masked.nrrd"]
-    call(command)
+command = [animaMaskImage, "-i", brainImage, "-m", brainImagePrefix + "_rough_brainMask.nrrd", "-o",
+           brainImagePrefix + "_rough_masked.nrrd"]
+call(command)
 
-    brainImageRoughMasked = brainImagePrefix + "_rough_masked.nrrd"
+brainImageRoughMasked = brainImagePrefix + "_rough_masked.nrrd"
+
+if args.second_step is True:
     # Fine mask with masked brain
     command = [animaPyramidalBMRegistration, "-m", atlasImageMasked, "-r", brainImageRoughMasked, "-o",
                brainImagePrefix + "_rig.nrrd", "-O", brainImagePrefix + "_rig_tr.txt", "--sp", "3"] + pyramidOptions
@@ -111,6 +120,9 @@ for brainImage in sys.argv[1:]:
     command = [animaMaskImage, "-i", brainImage, "-m", brainImagePrefix + "_brainMask.nrrd", "-o",
                brainImagePrefix + "_masked.nrrd"]
     call(command)
+else:
+    copyfile(brainImageRoughMasked,brainImagePrefix + "_masked.nrrd")
+    copyfile(brainImagePrefix + "_rough_brainMask.nrrd",brainImagePrefix + "_brainMask.nrrd")
 
 map(os.remove, glob.glob("*_rig*"))
 map(os.remove, glob.glob("*_aff*"))
