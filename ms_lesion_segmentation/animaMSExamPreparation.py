eb1a3fadd4d7bd8ab5989ca1d998b18cf0196527
Olivier Commowick <olivier.commowick@inria.fr>
2019-02-25 14:33:58 +0100

    Python 3 compatibility

--- ms_lesion_segmentation/animaMSExamPreparation.py
+++ ms_lesion_segmentation/animaMSExamPreparation.py
@@ -57,7 +57,7 @@ call(brainExtractionCommand)
 
 # Decide on whether to use large image setting or small image setting
 command = [animaConvertImage, "-i", refImage, "-I"]
-convert_output = check_output(command)
+convert_output = check_output(command, universal_newlines=True)
 size_info = convert_output.split('\n')[1].split('[')[1].split(']')[0]
 large_image = False
 for i in range(0, 3):
