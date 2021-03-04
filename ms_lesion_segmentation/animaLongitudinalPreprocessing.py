b2b93506031f25bd35738e7142042981dff50aed
Arthur Masson <arthur.sw@gmail.com>
2021-03-04 11:45:18 +0100

    take into account olivier's comment https://github.com/Inria-Visages/Anima-Scripts-Public/pull/12

--- ms_lesion_segmentation/animaLongitudinalPreprocessing.py
+++ ms_lesion_segmentation/animaLongitudinalPreprocessing.py
@@ -4,8 +4,6 @@ import configparser as ConfParser
 import argparse
 import subprocess
 import shutil
-from pathlib import Path
-Path.ls = lambda x: list(x.iterdir())
 
 # Data preprocessing for the Longitudinal Multipel Sclerosis Lesion Segmentation Challenge of MICCAI 2021.
 
@@ -13,7 +11,7 @@ Path.ls = lambda x: list(x.iterdir())
 #  - brain extraction
 #  - bias correction
 #  - (optional) normalization on the given template
-#  - crop from the union of brain masks of both time points
+#  - mask flair images with the union of the masks of both time points
 
 # Argument parsing
 parser = argparse.ArgumentParser(
@@ -47,31 +45,32 @@ parser.add_argument('-t', '--template', type=str, help='Path to the template ima
 
 args = parser.parse_args()
 
-patients = Path(args.input)
-templateFlair = Path(args.template) if args.template else None
-output = Path(args.output)
+patients = args.input
+templateFlair = args.template if args.template else None
+output = args.output
 
 # The configuration file for anima is ~/.anima/config.txt (can be overridden with -a and -s arguments)
-configFilePath = Path.home() / '.anima' / 'config.txt'
+configFilePath = os.path.join(os.path.expanduser("~"),'.anima', 'config.txt')
 
 # Open the configuration parser and exit if anima configuration cannot be loaded
 configParser = ConfParser.RawConfigParser()
 
-if configFilePath.exists():
+if os.path.exists(configFilePath):
     configParser.read(configFilePath)
 else:
     sys.exit('Please create a configuration file (~/.anima/config.txt) for Anima python scripts.')
 
 # Initialize anima directories
-animaDir = Path(configParser.get("anima-scripts", 'anima'))
-animaScriptsPublicDir = Path(configParser.get("anima-scripts", 'anima-scripts-public-root'))
+animaDir = configParser.get("anima-scripts", 'anima')
+animaScriptsPublicDir = configParser.get("anima-scripts", 'anima-scripts-public-root')
 
 # Anima commands
-animaBrainExtraction = animaScriptsPublicDir / "brain_extraction" / "animaAtlasBasedBrainExtraction.py"
-animaN4BiasCorrection = animaDir / "animaN4BiasCorrection"
-animaNyulStandardization = animaDir / "animaNyulStandardization"
-animaCropImage = animaDir / "animaCropImage"
-animaImageArithmetic = animaDir / "animaImageArithmetic"
+animaBrainExtraction = os.path.join(animaScriptsPublicDir, "brain_extraction", "animaAtlasBasedBrainExtraction.py")
+animaN4BiasCorrection = os.path.join(animaDir, "animaN4BiasCorrection")
+animaNyulStandardization = os.path.join(animaDir, "animaNyulStandardization")
+animaThrImage = os.path.join(animaDir, "animaThrImage")
+animaMaskImage = os.path.join(animaDir, "animaMaskImage")
+animaImageArithmetic = os.path.join(animaDir, "animaImageArithmetic")
 
 # Calls a command, if there are errors: outputs them and exit
 def call(command):
@@ -87,48 +86,60 @@ def call(command):
 #  - bias correction
 #  - normalize (optional)
 #  - crop
-for patient in patients.ls():
+for patientName in os.listdir(patients):
 
-    if not patient.is_dir(): continue
+    patient = os.path.join(patients, patientName)
 
-    print("Preprocessing patient " + patient.name + "...")
+    if not os.path.isdir(patient): continue
+
+    print("Preprocessing patient " + patientName + "...")
 
     # Create the output directory which will contain the preprocessed files
-    patientOutput = output / patient.name
-    patientOutput.mkdir(exist_ok=True, parents=True)
+    patientOutput = os.path.join(output, patientName)
+    
+    os.makedirs(patientOutput, exist_ok=True)
     
     masks = []
 
     # For both time points: extract brain and remove bias
     for flairName in ['flair_time01_on_middle_space.nii.gz', 'flair_time02_on_middle_space.nii.gz']:
         
-        flair = patient / flairName
-        brain = patientOutput / flairName
-        mask = patientOutput / flairName.replace('.nii.gz', '_mask.nii.gz')
+        flair = os.path.join(patient, flairName)
+        brain = os.path.join(patientOutput, flairName)
+        mask = os.path.join(patientOutput, flairName.replace('.nii.gz', '_mask.nii.gz'))
 
         # Extract brain
-        call(["python", animaBrainExtraction, "-i", flair, "-S", "--mask", mask, "--brain", brain])
+        call(["python", animaBrainExtraction, "-i", flair, "--mask", mask, "--brain", brain])
+
         # Remove bias
         call([animaN4BiasCorrection, "-i", brain, "-o", brain, "-B", "0.3"])
         
-        if templateFlair and templateFlair.exists():
+        if templateFlair and os.path.exists(templateFlair):
             # Normalize intensities with the given template
             call([animaNyulStandardization, "-m", flair, "-r", templateFlair, "-o", flair])
-        elif not templateFlair.exists():
-            print('Template file ' + str(templateFlair) + ' not found, skipping normalization.')
+        elif not os.path.exists(templateFlair):
+            print('Template file ' + templateFlair + ' not found, skipping normalization.')
         
         masks.append(mask)
     
-    mask = patientOutput / 'mask.nii.gz'
+    maskUnion = os.path.join(patientOutput, 'brain_mask.nii.gz')
 
     # Compute the union of the masks of both time points
-    call([animaImageArithmetic, "-i", masks[0], "-a", masks[1], "-o", mask])
+    call([animaImageArithmetic, "-i", masks[0], "-a", masks[1], "-o", maskUnion])    # add the two masks
+    call([animaThrImage, "-i", maskUnion, "-t", "0.5", "-o", maskUnion])                  # threshold to get a binary mask
 
+    # Remove intermediate masks
+    for mask in masks:
+        os.remove(mask)
+    
     # Copy the ground truths to the output directory
     for imageName in ['ground_truth_expert1.nii.gz', 'ground_truth_expert2.nii.gz', 'ground_truth_expert3.nii.gz', 'ground_truth_expert4.nii.gz', 'ground_truth.nii.gz']:
-        shutil.copyfile(patient / imageName, patientOutput / imageName)
+        shutil.copyfile(os.path.join(patient, imageName), os.path.join(patientOutput, imageName))
 
-    # Crop all output images with the computed mask
-    for image in patientOutput.ls():
-        call([animaCropImage, "-i", image, "-m", mask, "-o", image])
+    # Mask original FLAIR images with the union mask
+    for flairName in ['flair_time01_on_middle_space.nii.gz', 'flair_time02_on_middle_space.nii.gz']:
+        
+        flair = os.path.join(patient, flairName)
+        brain = os.path.join(patientOutput, flairName)
 
+        call([animaMaskImage, "-i", flair, "-m", maskUnion, "-o", brain])
